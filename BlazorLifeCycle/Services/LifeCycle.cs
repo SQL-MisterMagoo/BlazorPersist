612194d9f1962a5b6e7b84a11fb03a8ed6b93243
Mister Magoo <mister.magoo+githubsql@gmail.com>
2018-12-25 02:45:25 +0000

    Added support for non-nullable value types, but seem to have broken reference types, doh!

--- BlazorLifeCycle/Services/LifeCycle.cs
+++ BlazorLifeCycle/Services/LifeCycle.cs
@@ -9,94 +9,92 @@ namespace BlazorLifeCycle.Services
 {
 	public class LifeCycle
 	{
-		public static readonly Dictionary<string, object> appState = new Dictionary<string, object>();
-		public static readonly Dictionary<string, int> appStateTimeout = new Dictionary<string, int>();
+		public static readonly Dictionary<string, ILifeCycleCache> appState = new Dictionary<string, ILifeCycleCache>();
 
-		//private static async Task ReloadAppState()
-		//{
-		//	string[] cookie = await JSRuntime.Current.InvokeAsync<string[]>("blazorLifeCycle.listdata");
-		//	await ProcessCookies(cookie);
-		//}
-
-		//private static async Task ProcessCookies(string[] cookies)
-		//{
-		//	Console.WriteLine($"TEST: cookies={cookies}");
-		//	for (int i = 0; i < cookies.Length; i++)
-		//	{
-		//		string name = cookies[i];
-		//		string value = await JSRuntime.Current.InvokeAsync<string>("blazorLifeCycle.readdata", name);
-		//		Console.WriteLine($"TEST: name={name}, value={value}");
-		//		if (!string.IsNullOrWhiteSpace(name) && !string.IsNullOrWhiteSpace(value))
-		//		{
-		//			ExtractAndReHydrate(name, value);
-		//		}
-		//	}
-		//}
-
-		//private static void ExtractAndReHydrate(string name, string value)
-		//{
-		//	var names = name.Split('|');
-		//	var objName = names?[0];
-		//	var objType = names?[1];
-		//	Console.WriteLine($"TEST: objName={objName}, objType={objType}, value={value}");
-		//	if (!string.IsNullOrWhiteSpace(objType))
-		//	{
-		//		ReHydrateState(value, objName, objType);
-		//	}
-		//}
-
-		private static void ReHydrateState(string value, string objName, string objType)
-		{
-			Type type = Type.GetType(objType);
-			Console.WriteLine($"REHYDRATE: type={type?.FullName}");
-			var mi = typeof(Json).GetMethod("Deserialize");
-			var desRef = mi.MakeGenericMethod(type);
-			var result = desRef.Invoke(null, new[] { value });
-			Console.WriteLine($"REHYDRATE: result={result?.GetType().FullName}");
-			appState[objName] = result;
-		}
-
+		/// <summary>
+		/// This method should be hooked up by javascript to the window.beforeunload event.
+		/// It will then save all state to localstorage.
+		/// </summary>
+		/// <returns></returns>
 		[JSInvokable("onbeforeunload")]
 		public static string OnBeforeUnload()
 		{
-			Console.WriteLine("In LifeCycle - OnBeforeUnload");
-
 			if (appState == null)
 				return null;
 
 			foreach (var kvp in appState)
 			{
-				var timeout = appStateTimeout.ContainsKey(kvp.Key) ? appStateTimeout[kvp.Key] : 0;
-				//string timeoutString = timeout > 0 ? DateTime.UtcNow.AddDays(timeout).AddMinutes(-1).ToString("r") : null;
-
+				// This call ensures that we have the latest data for non-nullable value types
+				kvp.Value.UpdateData();
 				JSRuntime
 					.Current
-					.InvokeAsync<bool>("blazorLifeCycle.savedata", $"{kvp.Key}|{kvp.Value.GetType().FullName}", Json.Serialize(kvp.Value), timeout);
+					.InvokeAsync<bool>("blazorLifeCycle.savedata", kvp.Key, Json.Serialize( kvp.Value));
 			}
 			return null; //return a string to get a warning
 		}
 
-		public static void AddToAppState(string name, object value, int timeout=0)
+		public static void AddToAppState<T>(string name, T value, int timeout=0, Func<T> saveAction=null)
 		{
-			appState[name] = value;
-			appStateTimeout[name] = timeout;
+			appState[name] = new LifeCycleCache<T> { ObjectData = value, LifeTimeMinutes = timeout, SaveAction=saveAction };
 		}
 
-		public static async Task<T> GetAppState<T>(string name, int timeout=0) where T : class
+		/// <summary>
+		/// Retrieve data of type T from the App State
+		/// </summary>
+		/// <typeparam name="T">The type of value to store - can be class or a non-nullable value type such as int or string</typeparam>
+		/// <param name="name">The unique name for the data</param>
+		/// <param name="action">Only required for non-nullable value types such as "int".
+		/// This Func will be called when the browser window is closed or refreshed. 
+		/// It should return the current data to be saved.</param>
+		/// <returns></returns>
+		public static async Task<T> GetAppState<T>(string name, Func<T> action = null) 
 		{
-			Console.WriteLine($"LIFE: Looking for {name} AppState");
 			if (!appState.ContainsKey(name))
 			{
-				Console.WriteLine($"LIFE: Appstate for {name} not found");
-				string stName = $"{name}|{typeof(T).FullName}";
-				Console.WriteLine($"LIFE: Reading localstorage {stName}");
-				string value = await JSRuntime.Current.InvokeAsync<string>("blazorLifeCycle.readdata", stName);
-				Console.WriteLine($"LIFE: GOT localstorage {value}");
-				ReHydrateState(value, name, typeof(T).FullName);
+				var value = await JSRuntime.Current.InvokeAsync<string>("blazorLifeCycle.readdata", name);
+				appState[name] = Json.Deserialize<LifeCycleCache<T>>(value);
 			}
-			appStateTimeout[name] = timeout;
-			return appState[name] as T;
+			var localCache = appState[name] as LifeCycleCache<T>;
+			if (localCache.LifeTimeMinutes > 0 && localCache.GetExpiryDate() < DateTime.UtcNow)
+			{
+				appState.Remove(name);
+				return default(T);
+			}
+			localCache.SaveAction = action;
+			return localCache.ObjectData;
 		}
 
+	}
+	public interface ILifeCycleCache
+	{
+		void UpdateData();
+	}
+	[Serializable]
+	public class LifeCycleCache<T> : ILifeCycleCache
+	{
+		public DateTime cacheExpires;
+		public T ObjectData { get; set; }
+		public int LifeTimeMinutes { get; set; }
+		public Func<T> SaveAction { private get; set; }
+		public DateTime GetExpiryDate() 
+			=> cacheExpires == DateTime.MinValue ? DateTime.UtcNow.AddMinutes(LifeTimeMinutes) : cacheExpires;
+		public LifeCycleCache()
+		{
+
+		}
+		public string AsJson()
+		{
+			return Json.Serialize(this);
+		}
+
+		public void UpdateData()
+		{
+			cacheExpires = GetExpiryDate();
+			if (SaveAction == null)
+			{
+				return;
+			}
+			ObjectData = SaveAction();
+		}
 	}
 }
--- BlazorLifeCycle/wwwroot/js/blazorLifeCycle.js
+++ BlazorLifeCycle/wwwroot/js/blazorLifeCycle.js
@@ -2,40 +2,17 @@
 
 window.blazorLifeCycle = {
 
-    savedata: function (name, value, timeout) {
+    savedata: function (name, value) {
         if (name && value) {
+            console.log("JS:SAVE:" + name);
             localStorage.setItem(name, value);
-            console.log("JS:SAVE:" + name + "," + value + "," + timeout);
-            if (timeout > 0) {
-                const expireDate = new Date(Date.now() + timeout * 60000);
-                console.log("JS:SAVE: Expires : " + expireDate.toLocaleDateString());
-                const data = JSON.stringify({ "expires": expireDate.toISOString(), "timeout": timeout });
-                console.log("JS:SAVE: Expires data :" + data);
-                localStorage.setItem(name + EXP_NAME, data );
-            }
+            return false;
         }
         return true;
     },
     readdata: function (name) {
-        const expires = localStorage.getItem(name + EXP_NAME);
-        console.log("JS:READ:" + name + " expires=" + expires);
-        if (expires) {
-            const data = JSON.parse(expires);
-            const dateExpires = new Date( Date.parse( data.expires ) );
-            console.log("JS:READ:" + name + " dateExpires=" + dateExpires.toTimeString());
-            if (dateExpires) {
-                if (dateExpires < Date.now()) {
-                    console.log("JS:READ:Removing expired " + name);
-                    localStorage.removeItem(name);
-                    localStorage.removeItem(name + EXP_NAME);
-                }
-            }
-        }
-        console.log("JS:READ:Reading " + name);
+        console.log("JS:READ: " + name);
         return localStorage.getItem(name);
-    },
-    listdata: function () {
-        return Object.keys(localStorage);
     }
 };
 window.onbeforeunload = function () {
