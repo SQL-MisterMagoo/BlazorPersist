a5d615ef1e4eade4560b7bb31b45b9b7f1fbcc17
Mister Magoo <mister.magoo+githubsql@gmail.com>
2018-12-28 00:13:34 +0000

    Added list sample

---
+++ BlazorPersist/Models/ISampleData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPersist.Models
+{
+	public interface ISampleData
+	{
+		int Id { get; }
+		string Name { get; set; }
+		string NickName { get; set; }
+	}
+}
